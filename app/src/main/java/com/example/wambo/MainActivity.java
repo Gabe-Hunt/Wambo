32f3ba52fb63ec4f638dd3165dd763f2b2327f88
Gabe-Hunt <gabehunt2017@gmail.com>
2019-04-15 14:36:28 -0400

    First commit. App has splash screen and outlines for a few methods.

---
+++ app/src/main/java/com/example/wambo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.wambo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * onClick method for launch button
+     * Takes user to desired soundboard
+     */
+    public void chooseSoundboard() {
+
+    }
+
+    /**
+     * onClick method for 'Create new Soundboard' button
+     * Directs user away from splash screen and makes new board
+     */
+    public void createNewSoundboard() {
+
+    }
+
+    
+
+}
