2228f0716e1924518499018ff9a3e6101dd64350
Gabe-Hunt <gabehunt2017@gmail.com>
2019-04-22 12:36:54 -0400

    Added sound files and created xml file for soundBoardPage

--- app/src/main/java/com/example/wambo/MainActivity.java
+++ app/src/main/java/com/example/wambo/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.wambo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner dropDownMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dropDownMenu = findViewById(R.id.dropDownMenu);
+        Button newSoundboardButton = findViewById(R.id.createNewSoundboardButton);
+
+        newSoundboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewSoundboard();
+            }
+        });
     }
 
     /**
@@ -24,9 +41,10 @@ public class MainActivity extends AppCompatActivity {
      * Directs user away from splash screen and makes new board
      */
     public void createNewSoundboard() {
-
+        Intent intent = new Intent(MainActivity.this, soundboardPage.class);
+        startActivity(intent);
     }
 
-    
+
 
 }
---
+++ app/src/main/java/com/example/wambo/soundboardPage.java
@@ -0,0 +1,24 @@
+package com.example.wambo;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class soundboardPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soundboard_page);
+        final MediaPlayer brekfastSound =MediaPlayer.create(this, R.raw.brekfast);
+        Button brekfastButton = findViewById(R.id.soundBoardButton1);
+        brekfastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                brekfastSound.start();
+            }
+        });
+    }
+}
