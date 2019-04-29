b06f19f92fe8272a6f7b0ff05f6739e29c974634
Gabe-Hunt <gabehunt2017@gmail.com>
2019-04-29 15:26:00 -0400

    Working on programmatically creating and assigning values to the buttons

--- app/src/main/java/com/example/wambo/soundboardPage.java
+++ app/src/main/java/com/example/wambo/soundboardPage.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -16,6 +18,8 @@ public class soundboardPage extends AppCompatActivity {
     ArrayList<Button> buttonList;
     ArrayList<MediaPlayer> soundsList;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,44 +27,47 @@ public class soundboardPage extends AppCompatActivity {
 
         buttonList = new ArrayList<>();
 
-        final MediaPlayer brekfastSound = MediaPlayer.create(this, R.raw.brekfast);
-        final MediaPlayer bruhSound = MediaPlayer.create(this, R.raw.bruh);
-        final MediaPlayer chickensSound = MediaPlayer.create(this, R.raw.chickens);
-        final MediaPlayer imFineSound = MediaPlayer.create(this, R.raw.im_fine);
-        final MediaPlayer rawSound = MediaPlayer.create(this, R.raw.raw);
-        final MediaPlayer runSound = MediaPlayer.create(this, R.raw.run);
-        final MediaPlayer twentyOneSound = MediaPlayer.create(this, R.raw.twentyone);
-        final MediaPlayer vodkaSound = MediaPlayer.create(this, R.raw.vodka);
-        final MediaPlayer wamSound = MediaPlayer.create(this, R.raw.wam);
-        final MediaPlayer wednesdaySound = MediaPlayer.create(this, R.raw.wednesday);
+        MediaPlayer sound1 = MediaPlayer.create(this, R.raw.brekfast);
+        MediaPlayer sound2 = MediaPlayer.create(this, R.raw.bruh);
+        MediaPlayer sound3 = MediaPlayer.create(this, R.raw.chickens);
+        MediaPlayer sound4 = MediaPlayer.create(this, R.raw.im_fine);
+        MediaPlayer sound5 = MediaPlayer.create(this, R.raw.raw);
+        MediaPlayer sound6 = MediaPlayer.create(this, R.raw.run);
+        MediaPlayer sound7 = MediaPlayer.create(this, R.raw.twentyone);
+        MediaPlayer sound8 = MediaPlayer.create(this, R.raw.vodka);
+        MediaPlayer sound9 = MediaPlayer.create(this, R.raw.wam);
+        MediaPlayer sound10 = MediaPlayer.create(this, R.raw.wednesday);
+
 
         soundsList = new ArrayList<>();
-        soundsList.add(brekfastSound);
-        soundsList.add(bruhSound);
-        soundsList.add(chickensSound);
-        soundsList.add(imFineSound);
-        soundsList.add(rawSound);
-        soundsList.add(runSound);
-        soundsList.add(twentyOneSound);
-        soundsList.add(vodkaSound);
-        soundsList.add(wamSound);
-        soundsList.add(wednesdaySound);
+        soundsList.add(sound1);
+        soundsList.add(sound2);
+        soundsList.add(sound3);
+        soundsList.add(sound4);
+        soundsList.add(sound5);
+        soundsList.add(sound6);
+        soundsList.add(sound7);
+        soundsList.add(sound8);
+        soundsList.add(sound9);
+        soundsList.add(sound10);
 
         int[] buttonID = {R.id.soundBoardButton1, R.id.soundBoardButton2, R.id.soundBoardButton3 ,R.id.soundBoardButton4, R.id.soundBoardButton5, R.id.soundBoardButton6, R.id.soundBoardButton7, R.id.soundBoardButton8, R.id.soundBoardButton9, R.id.soundBoardButton10 };
 
+        Button b1 = findViewById(R.id.soundBoardButton1);
 
-        for(int i = 0; i < 10; i++) {
-            Button button = findViewById(buttonID[i]);
-            buttonList.add(button);
-            addSound(button);
+        b1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+//                Intent intent = new Intent(soundboardPage.this, PopupWindow.class );
+//                startActivity(intent);
+                AlertDialog.Builder builder = new AlertDialog.Builder(soundboardPage.this);
 
-        }
 
+                return true;
+            }
+        });
 
-        Button button = new Button(this);
-        button.setId(R.id.soundBoardButton1);
-        button.setText("testy");
-
+        /*
         Button brekfastButton = findViewById(R.id.soundBoardButton1);
         brekfastButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +75,7 @@ public class soundboardPage extends AppCompatActivity {
                 brekfastSound.start();
             }
         });
+        */
 
     }
 
@@ -79,16 +87,46 @@ public class soundboardPage extends AppCompatActivity {
 
     }
 
-    public void createButtons() {
-        for(int i = 0; i < 16; i++) {
-            Button button = new Button(this);
-            buttonList.add(button);
-
-        }
-    }
 
     public void playSound(View view) {
 //        This will be the onClick method
 //        Using if statements
+        switch(view.getId()) {
+            case R.id.soundBoardButton1:
+                soundsList.get(0).start();
+                break;
+
+            case R.id.soundBoardButton2:
+                soundsList.get(1).start();
+                break;
+            case R.id.soundBoardButton3:
+                soundsList.get(2).start();
+                break;
+            case R.id.soundBoardButton4:
+                soundsList.get(3).start();
+                break;
+            case R.id.soundBoardButton5:
+                soundsList.get(4).start();
+                break;
+            case R.id.soundBoardButton6:
+                soundsList.get(5).start();
+                break;
+            case R.id.soundBoardButton7:
+                soundsList.get(6).start();
+                break;
+            case R.id.soundBoardButton8:
+                soundsList.get(7).start();
+                break;
+            case R.id.soundBoardButton9:
+                soundsList.get(8).start();
+                break;
+            case R.id.soundBoardButton10:
+                soundsList.get(9).start();
+                break;
+            default:
+                break;
+        }
+
+
     }
 }
