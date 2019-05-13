a1d9c6e3afaf41ed9043db46bfea36cecf10e70c
Gabe-Hunt <gabehunt2017@gmail.com>
2019-05-13 04:18:41 -0400

    Added recording feature. Buttons were changed to object oriented. Added 6 more sound effects. Users can now use custom sounds.

--- app/src/main/java/com/example/wambo/MainActivity.java
+++ app/src/main/java/com/example/wambo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.wambo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -14,11 +15,15 @@ public class MainActivity extends AppCompatActivity {
     Spinner dropDownMenu;
     Intent intent;
     Button createNewSoundboardButton;
+    String boardSelection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Spinner
         dropDownMenu = findViewById(R.id.dropDownMenu);
+        //Spinner value to string
+        boardSelection = dropDownMenu.getSelectedItem().toString();
         createNewSoundboard();
     }
 
@@ -26,7 +31,36 @@ public class MainActivity extends AppCompatActivity {
      * onClick method for launch button
      * Takes user to desired soundboard
      */
-    public void chooseSoundboard(View view) {
+    /**
+     * onClick method for launch button
+     * Takes user to desired soundboard
+     */
+    public void chooseSoundboard(View v) {
+
+        try {
+            if (boardSelection.equals("Default Board")) {
+                intent = new Intent(MainActivity.this, soundboardPage.class);
+                this.startActivity(intent);
+
+            } else if (boardSelection.equals("Custom Board 1")) {
+                intent = new Intent(this, soundboardPageTwo.class);
+                this.startActivity(intent);
+
+            } else if (boardSelection.equals("Custom Board 2")) {
+                intent = new Intent(this, soundboardPageThree.class);
+                this.startActivity(intent);
+
+            } else if (boardSelection.equals("Custom Board 3")) {
+                intent = new Intent(this, soundboardPageFour.class);
+                this.startActivity(intent);
+
+            } else if (boardSelection.equals("Custom Board 4")) {
+                intent = new Intent(this, soundboardPageFive.class);
+                this.startActivity(intent);
+            }
+        } catch (Exception e) {
+            Log.e("MyActivity::MyMethod", e.getMessage());
+        }
 
     }
 
